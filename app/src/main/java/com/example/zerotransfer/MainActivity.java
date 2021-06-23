3ae911a206615466c5b0e396f5d7ee81a21d2251
Liquid-Aristocracy <Na1048576@pm.me>
2021-06-23 21:34:56 +0800

    init

---
+++ app/src/main/java/com/example/zerotransfer/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.zerotransfer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.zerotier.sockets.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
